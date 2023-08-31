ab1c565cf1b362ad7045a95294a2d9899f60e81c
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2023-08-31 22:37:43 +0545

    Completed labs

---
+++ lab3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter :";
+    cin>>n;
+    int x = rand();
+    cout<<x;
+
+}
